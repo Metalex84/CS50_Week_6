809b0fe446da0529395ac8b85b70016485088911
AGV <agonzalez.venegas@outlook.com>
2024-05-30 19:26:28 +0200

    Sentimental Mario More Py

---
+++ sentimental-mario-more/mario.py
@@ -0,0 +1,25 @@
+def main():
+
+    while True:
+        try:
+            height = int(input("Height: "))
+            if height < 1 or height > 8:
+                print("Only values between 1 and 8")
+            else:
+                break
+        except ValueError:
+            print("Only numbers allowed")
+
+    for i in range(height):
+        for j in range(height - i - 1):
+            print(" ", end="")
+        for j in range(i + 1):
+            print("#", end="")
+        print("  ", end="")
+        for j in range(i + 1):
+            print("#", end="")
+        print()
+
+
+if __name__ == "__main__":
+    main()
